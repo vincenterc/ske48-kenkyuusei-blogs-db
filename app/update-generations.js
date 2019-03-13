44bcc9455f26831507c86be3ec1b437f3b43783e
vincenterr <chung.menghsun@gmail.com>
2019-03-13 19:32:20 +0800

    Add update-generations

---
+++ app/update-generations.js
@@ -0,0 +1,22 @@
+const fs = require('fs')
+const mongoose = require('mongoose')
+
+const { dbPath } = require('./constants')
+const {
+  readGenerationDetailList,
+} = require('./controllers/generation-controller')
+
+const fileName = 'members.json'
+
+const main = async () => {
+  mongoose.connect(dbPath, { useNewUrlParser: true })
+
+  let generations = await readGenerationDetailList()
+  let generationsFiltered = generations.filter(g => g.members.length !== 0)
+
+  fs.writeFileSync(fileName, JSON.stringify(generationsFiltered, null, 2))
+
+  mongoose.connection.close()
+}
+
+main()
