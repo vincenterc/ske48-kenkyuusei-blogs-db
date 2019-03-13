b7ad965f9eacab301093db74d64fbd3dcbdcbcb6
vincenterr <chung.menghsun@gmail.com>
2019-03-13 19:12:09 +0800

    Add virtual field, members, in generation model

--- app/models/generation.js
+++ app/models/generation.js
@@ -2,9 +2,19 @@ const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema
 
-const GenerationSchema = new Schema({
-  identity: String,
-  title: String,
+const GenerationSchema = new Schema(
+  {
+    identity: String,
+    title: String,
+  },
+  { id: false, toJSON: { virtuals: true }, toObject: { virtuals: true } }
+)
+
+GenerationSchema.virtual('members', {
+  ref: 'Member',
+  localField: 'identity',
+  foreignField: 'generation',
+  justOne: false,
 })
 
 module.exports = mongoose.model('Generation', GenerationSchema)
