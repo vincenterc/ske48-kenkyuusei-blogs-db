bb7aa59698ab9f5d7f142d44e8f4eb44680b334b
vincenterr <chung.menghsun@gmail.com>
2019-03-13 19:14:59 +0800

    Add virtual fields, name_eng and posts, in member model

--- app/models/member.js
+++ app/models/member.js
@@ -2,10 +2,26 @@ const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema
 
-const MemberSchema = new Schema({
-  identity: String,
-  name: String,
-  generation: String,
+const MemberSchema = new Schema(
+  {
+    identity: String,
+    name: String,
+    generation: String,
+  },
+  { id: false, toJSON: { virtuals: true }, toObject: { virtuals: true } }
+)
+
+MemberSchema.virtual('name_eng').get(function() {
+  let temp = this.identity.split('_')
+  return `${temp[1]} ${temp[0]}`
+})
+
+MemberSchema.virtual('posts', {
+  ref: 'Post',
+  localField: 'identity',
+  foreignField: 'writer_identity',
+  justOne: false,
+  options: { sort: { identity: -1 } },
 })
 
 module.exports = mongoose.model('Member', MemberSchema)
